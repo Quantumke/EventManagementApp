8d2eb7f6704ef8f18195629882f41d29ae7e0027
Benson Mburu <nguruben@gmail.com>
2015-10-27 13:14:29 +0300

    Java splash screen

--- app/src/main/java/ml/dev2dev/eventregistrationapp/Splash.java
+++ app/src/main/java/ml/dev2dev/eventregistrationapp/Splash.java
@@ -1,6 +1,8 @@
 package ml.dev2dev.eventregistrationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +12,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Splash extends AppCompatActivity {
+    //add splash time out
+    public static int SPLASH_TIME_OUT=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+         /*
+          * Showing splash screen with a timer. This will be useful when you
+          * want to show case your app logo / company
+          */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, Login.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 
     }
 
