5cff0964158ed1fa55aeb1a242bca24661921d3b
Benson Mburu <nguruben@gmail.com>
2015-10-27 14:25:50 +0300

    INtents defined

--- app/src/main/java/ml/dev2dev/eventregistrationapp/Login.java
+++ app/src/main/java/ml/dev2dev/eventregistrationapp/Login.java
@@ -1,29 +1,71 @@
 package ml.dev2dev.eventregistrationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    Button facebook, gplus, twitter;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        login = (TextView) findViewById(R.id.login);
+        gplus = (Button) findViewById(R.id.gplus);
+        twitter = (Button) findViewById(R.id.twitter);
+        facebook = (Button) findViewById(R.id.facebook);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent a = new Intent(Login.this, MainActivity.class);
+                startActivity(a);
+                finish();
+
             }
         });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Login.this, MainActivity.class);
+                startActivity(a);
+                finish();
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Login.this, MainActivity.class);
+                startActivity(a);
+                finish();
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Login.this, MainActivity.class);
+                startActivity(a);
+                finish();
+
+            }
+        });
+
+
+
     }
 
 }
