beb119c1ecd356239591b9428fb9db74b79c6d34
Benson Mburu <nguruben@gmail.com>
2015-10-30 16:42:20 +0300

    render fixed

--- app/src/main/java/ml/dev2dev/eventregistrationapp/MainActivity.java
+++ app/src/main/java/ml/dev2dev/eventregistrationapp/MainActivity.java
@@ -12,18 +12,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 ImageView tweetimg;
-    TextView textView7;
-    TextView textView8;
-    TextView textView12;
+    TextView speaker;
+    TextView agenda;
+    TextView information;
+    TextView twitter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView8 =(TextView) findViewById(R.id.textView8);
-        textView7 = (TextView) findViewById(R.id.textView7);
-        tweetimg = (ImageView) findViewById(R.id.tweetimg);
-        textView12 = (TextView) findViewById(R.id.textView12);
-        tweetimg.setOnClickListener(new View.OnClickListener() {
+        speaker =(TextView) findViewById(R.id.speaker);
+        agenda = (TextView) findViewById(R.id.agenda);
+        information = (TextView) findViewById(R.id.info);
+        twitter = (TextView) findViewById(R.id.twitter);
+
+        twitter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent b = new Intent(MainActivity.this, Twitter.class);
@@ -32,7 +34,7 @@ ImageView tweetimg;
             }
         });
 
-        textView7.setOnClickListener(new View.OnClickListener() {
+        speaker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent m = new Intent(MainActivity.this, actors.class);
@@ -40,7 +42,7 @@ ImageView tweetimg;
                 finish();
             }
         });
-        textView8.setOnClickListener(new View.OnClickListener() {
+        agenda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -49,7 +51,7 @@ ImageView tweetimg;
                 finish();
             }
         });
-        textView12.setOnClickListener(new View.OnClickListener() {
+        information.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent n = new Intent(MainActivity.this, information.class);
